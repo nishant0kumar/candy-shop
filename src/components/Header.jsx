facedff2bf076ca853a599f26efcf6387764a1c4
Nishant Kumar <nishant51121@gmail.com>
2025-01-13 15:34:44 +0530

    Refactor Header component: Improved layout and navigation links with icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,34 @@
 import '../assets/css/common.css';
-
 import {Link} from "react-router-dom";
-import CartButton  from '../assets/extras/CartButton.jsx';
-export default function Header(props) {
-    
-    return(
-      <header>
-          <div className="loogo">
-             <a href="/">
-             <img src="https://www.iconeasy.com/icon/256/System/Swirl%20Finder/Finder%20Candy.png" alt="logo" height="50px" width="100px"/>
-             </a>
-          </div>
+import CartButton from '../assets/extras/CartButton.jsx';
 
-          <nav className="nav">
-            <a href={props.title} className="nav-link">{props.title}</a>
-            <a href="/favour" className="nav-link">Love</a>
-            <div className="js-cart-container">
-              <CartButton/>
-                
+export default function Header(props) {
+    return(
+        <header>
+            <div className="header-left">
+                <Link to="/" className="logo">
+                    <img src="https://www.iconeasy.com/icon/256/System/Swirl%20Finder/Finder%20Candy.png" alt="logo"/>
+                    <span className="brand-name">CandyShop</span>
+                </Link>
             </div>
-          </nav>
-      </header>
+
+            <nav className="nav">
+                <Link to="/" className="nav-link">
+                    <i className="fas fa-home"></i>
+                    <span>Home</span>
+                </Link>
+                <Link to="/store" className="nav-link">
+                    <i className="fas fa-store"></i>
+                    <span>Store</span>
+                </Link>
+                <Link to="/favour" className="nav-link">
+                    <i className="fas fa-heart"></i>
+                    <span>Favorites</span>
+                </Link>
+                <div className="js-cart-container">
+                    <CartButton/>
+                </div>
+            </nav>
+        </header>
     );
 }
